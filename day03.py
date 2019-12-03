89d40730ee00d381c797667d366383019edab4f3
Peter "Rocky" Villano <peter.s.villano@gmail.com>
2019-12-03 10:39:14 -0500

    3 pt 1

--- day03.py
+++ day03.py
@@ -2,10 +2,41 @@
 from math import *
 from itertools import *
 
+dirs = {
+    'L': (-1, 0),
+    'R': (1,0),
+    'U': (0,1),
+    'D': (0,-1),
+}
+
 
 with open('day03.txt') as f:
-    for line in f:
-        for token in line.split(', '):
-            answer = token
-            
-print(answer)
+    lines = list(f)
+    curpt = [0,0]
+    pts = []
+    for token in lines[0].split(','):
+        direction = token[0]
+        dist = int(token[1:])
+        for i in range(dist):
+            curpt[0] += dirs[direction][0]
+            curpt[1] += dirs[direction][1]
+            pts.append(tuple(curpt))
+    pts = set(pts)
+
+
+    curpt = [0,0]
+    best = (10**10,0)
+    for token in lines[1].split(','):
+        direction = token[0]
+        dist = int(token[1:])
+        for i in range(dist):
+            curpt[0] += dirs[direction][0]
+            curpt[1] += dirs[direction][1]
+            my = tuple(curpt)
+            if my in pts:
+                if sum(map(abs, my)) < sum(map(abs, best)):
+                    best = my
+                    print(best, sum(map(abs, best)))
+
+
+
