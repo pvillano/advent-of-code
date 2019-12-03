8a6d7a99249ae5ef3a04816e8f5ad4159efa74fe
Peter "Rocky" Villano <peter.s.villano@gmail.com>
2019-12-03 10:44:25 -0500

    day 3 pt 2

--- day03.py
+++ day03.py
@@ -13,30 +13,33 @@ dirs = {
 with open('day03.txt') as f:
     lines = list(f)
     curpt = [0,0]
-    pts = []
+    cur_dist = 0
+    pts = {}
     for token in lines[0].split(','):
         direction = token[0]
         dist = int(token[1:])
         for i in range(dist):
+            cur_dist += 1
             curpt[0] += dirs[direction][0]
             curpt[1] += dirs[direction][1]
-            pts.append(tuple(curpt))
-    pts = set(pts)
+            pts[tuple(curpt)] = cur_dist
 
 
     curpt = [0,0]
-    best = (10**10,0)
+    best = 10**10
+    cur_dist = 0
     for token in lines[1].split(','):
         direction = token[0]
         dist = int(token[1:])
         for i in range(dist):
+            cur_dist += 1
             curpt[0] += dirs[direction][0]
             curpt[1] += dirs[direction][1]
             my = tuple(curpt)
             if my in pts:
-                if sum(map(abs, my)) < sum(map(abs, best)):
-                    best = my
-                    print(best, sum(map(abs, best)))
+                if cur_dist + pts[my] < best:
+                    best = cur_dist + pts[my]
+                    print(best)
 
 
 
