3a235ce2cda35af8b7f1e94aa5a93851fab1b5ef
Peter "Rocky" Villano <peter.s.villano@gmail.com>
2020-01-02 17:54:02 -0500

    Update day06.py

--- day06.py
+++ day06.py
@@ -10,9 +10,28 @@ with open('day06.txt') as f:
         children[parent].append(child)
 
 
+def recurse_person(node, person):
+    if person in children[node]:
+        return 1
+    for i in (recurse_person(child, person) for child in children[node]):
+        if i:
+            return i + 1
+    return False
+
 def recurse(node, depth):
-    return sum(recurse(child, depth + 1) for child in children[node]) + depth
+    for i in (recurse(child, depth + 1) for child in children[node]):
+        if i:
+            return i
+    if not children[node]:
+        return 0
+    d_you = max(recurse_person(child, 'YOU') for child in children[node])
+    d_san = max(recurse_person(child, 'SAN') for child in children[node])
+    if d_you and d_san:
+        return d_you + d_san
+    else:
+        return 0
+
+
 
 
 print(recurse('COM', 0))
-print(children['COM'])
