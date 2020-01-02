12f7271547c854fd529f07c6c98054d1644541b0
Peter "Rocky" Villano <peter.s.villano@gmail.com>
2020-01-02 17:37:33 -0500

    day6

--- day06.py
+++ day06.py
@@ -1,11 +1,18 @@
 
 from math import *
 from itertools import *
+from collections import *
 
-
+children = defaultdict(list)
 with open('day06.txt') as f:
     for line in f:
-        for token in line.split(', '):
-            answer = token
-            
-print(answer)
+        parent, child, = line.strip().split(')')
+        children[parent].append(child)
+
+
+def recurse(node, depth):
+    return sum(recurse(child, depth + 1) for child in children[node]) + depth
+
+
+print(recurse('COM', 0))
+print(children['COM'])
