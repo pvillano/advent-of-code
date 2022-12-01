d09de89da52be0d9bd2ab779c4b8a83a6148e335
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-01 17:42:35 -0500

    day uno

--- day01.py
+++ day01.py
@@ -18,15 +18,19 @@ from utils import benchmark, debug_print, get_day, pipe
 
 test = """"""
 
-lines = get_day(1, test).split("\n")
+elves = get_day(1, test).split("\n\n")
+bags = [[int(line) for line in elf.split("\n")] for elf in elves]
 
 
 def part1():
-    pass
+    return max([sum(x) for x in bags])
 
 
 def part2():
-    pass
+    elf_totals = [sum(x) for x in bags]
+    best_elves = sorted(elf_totals)
+    sum_top = sum(best_elves[-3:])
+    return sum_top
 
 
 benchmark(part1)
