d8df3fcdbced0f8e0b7cd3b468c3383394e5a068
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-06 00:22:14 -0500

    day06.py fun

--- day06.py
+++ day06.py
@@ -1,20 +1,9 @@
-from collections import defaultdict, deque, Counter
-from copy import copy, deepcopy
-from functools import cache, lru_cache, partial, reduce
 from itertools import (
-    accumulate,
     count,
-    cycle,
-    product,
-    permutations,
-    combinations,
-    pairwise,
+    dropwhile,
 )
-from math import sqrt, floor, ceil, gcd, sin, cos, atan2
 
-from otqdm import otqdm
-
-from utils import benchmark, debug_print, get_day, pipe
+from utils import benchmark, get_day
 
 test = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
 
@@ -24,15 +13,24 @@ line = get_day(6, test)
 def part1():
     for idx, abcd in enumerate(zip(line[0:], line[1:], line[2:], line[3:])):
         if len(set(abcd)) == 4:
-            return idx+4
+            return idx + 4
 
 
 def part2():
     for idx, abcd in enumerate(zip(*[line[i:] for i in range(14)])):
         if len(set(abcd)) == 14:
-            return idx+14
+            return idx + 14
+
+
+def extra():
+    return next(
+        dropwhile(
+            lambda s: len(set(s)) <= 14, zip(count(14), *[line[i:] for i in range(14)])
+        )
+    )[0]
 
 
 if __name__ == "__main__":
     benchmark(part1)
     benchmark(part2)
+    benchmark(extra)
