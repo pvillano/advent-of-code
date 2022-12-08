0768857f54fd4a3684ca39d2d5f40b3a51d83b14
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-08 00:46:38 -0500

    cleaned more

--- day08.py
+++ day08.py
@@ -12,13 +12,10 @@ raw = get_day(8, test)
 lines = raw.split("\n")
 
 
-def gridify():
-    return [list(map(int, line)) for line in lines]
-
-
 def part1():
-    rows = len(lines)
-    cols = len(lines[0])
+    data = [list(map(int, line)) for line in lines]
+    rows = len(data)
+    cols = len(data[0])
     visible = [[False] * cols for _ in range(rows)]
     index_sets = (
         [[(i, j) for i in range(rows)] for j in range(cols)]
@@ -26,7 +23,6 @@ def part1():
         + [[(i, j) for j in range(cols)] for i in range(rows)]
         + [[(i, j) for j in reversed(range(cols))] for i in range(rows)]
     )
-    data = gridify()
     for index_set in index_sets:
         highest_seen = -1
         for i, j in index_set:
@@ -38,15 +34,15 @@ def part1():
 
 
 def part2():
-    rows = len(lines)
-    cols = len(lines[0])
-    data = gridify()
+    data = [list(map(int, line)) for line in lines]
+    rows = len(data)
+    cols = len(data[0])
 
     def generator():
         for i, j in product(range(rows), range(cols)):
             highest_allowed = data[i][j]
             score = 1
-            for d_i, d_j in ((0, -1), (0, 1), (-1, 0), (1, 0)):
+            for d_i, d_j in ((1, 0), (0, 1), (-1, 0), (0, -1)):
                 visible_cnt = 0
                 for k in count(1):
                     i2, j2 = i + k * d_i, j + k * d_j
