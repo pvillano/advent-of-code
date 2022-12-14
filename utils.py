0a14f85063e6564a3e13d102bb8f65ed44cff8d1
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-14 17:15:34 -0500

    only generate the next day's template if it is close to midnight

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ import inspect
 import os
 import sys
 import time
+import datetime
 from itertools import chain
 from pprint import pprint as not_my_pp
 from typing import Any, Callable
@@ -156,7 +157,7 @@ def benchmark(part: Callable) -> None:
     start_time = time.perf_counter_ns()
     ans = part()
     end_time = time.perf_counter_ns()
-    seconds = (end_time - start_time) / 10**9
+    seconds = (end_time - start_time) / 10 ** 9
     if DEBUG:
         pprint(ans, stream=sys.stderr)
         print(
@@ -167,19 +168,28 @@ def benchmark(part: Callable) -> None:
         print(f"completed in {seconds:0.3f} seconds\n")
 
 
-if __name__ == "__main__":
+def main():
     with open("template.py") as template_file:
-        t = time.localtime()
-        template_string = template_file.read()
+        eric_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-5), "EST"))
 
-        if t.tm_mon == 12:
-            up_to_day = t.tm_mday + 1
+        # only generate the next day's template if it is close to midnight
+        if eric_time.month == 12:
+            if eric_time.hour < 23:
+                up_to_day = eric_time.day
+            else:
+                up_to_day = eric_time.day + 1
         else:
             up_to_day = 25
 
+        template_string = template_file.read()
+        # inclusive range
         for i in range(1, up_to_day + 1):
             p = f"day{i:02}.py"
             if not os.path.exists(p):
                 file_contents = template_string.replace("DAYNUMBER", str(i))
                 with open(p, "x") as out_file:
                     out_file.write(file_contents)
+
+
+if __name__ == "__main__":
+    main()
