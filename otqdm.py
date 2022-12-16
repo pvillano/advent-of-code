1fae949421af5b6868f3264e6497bd2330a9791a
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-16 14:52:43 -0500

    reduced nesting of ifs

--- otqdm.py
+++ otqdm.py
@@ -45,67 +45,69 @@ def otqdm(
     for obj in iterator:
         yield obj
         n += 1
-        if n - last_print_n >= min_iters or n == len_iterator:
-            delta_t = time() - last_print_t
-            if delta_t >= min_interval or n == len_iterator:
-                now = time()
-                elapsed = now - start_time
-                elapsed_str = format_interval(elapsed)
-                rate = n / elapsed if elapsed > 0 else 0
+        delta_t = time() - last_print_t
+        normal_print = (n - last_print_n >= min_iters) and delta_t >= min_interval
+        override_print = n == len_iterator or n == 1
+        if not (normal_print or override_print):
+            continue
+        now = time()
+        elapsed = now - start_time
+        elapsed_str = format_interval(elapsed)
+        rate = n / elapsed if elapsed > 0 else 0
 
-                elapsed_prev = last_print_t - start_time
+        elapsed_prev = last_print_t - start_time
 
-                if last_print_n and elapsed_prev:
-                    exponent = log(elapsed / elapsed_prev) / log(n / last_print_n)
-                    if 0.05 <= exponent < 9.95:
-                        k = elapsed / (n**exponent)
-                        big_o_str = f"O(n^{exponent:3.1f})"
-                        if len_iterator is not None:
-                            remaining = k * (len_iterator**exponent) - elapsed
-                    else:
-                        base = exp(log(elapsed / elapsed_prev) / (n - last_print_n))
-                        if 0.05 <= base < 9.95:
-                            k = elapsed / (base**n)
-                            big_o_str = f"O({base:3.1f}^n)"
-                            if len_iterator is not None:
-                                remaining = k * (base**len_iterator) - elapsed
-                        else:
-                            big_o_str = "O(?????)"
-                            remaining = None
+        if last_print_n and elapsed_prev:
+            exponent = log(elapsed / elapsed_prev) / log(n / last_print_n)
+            if 0.05 <= exponent < 9.95:
+                k = elapsed / (n**exponent)
+                big_o_str = f"O(n^{exponent:3.1f})"
+                if len_iterator is not None:
+                    remaining = k * (len_iterator**exponent) - elapsed
+            else:
+                base = exp(log(elapsed / elapsed_prev) / (n - last_print_n))
+                if 0.05 <= base < 9.95:
+                    k = elapsed / (base**n)
+                    big_o_str = f"O({base:3.1f}^n)"
+                    if len_iterator is not None:
+                        remaining = k * (base**len_iterator) - elapsed
                 else:
                     big_o_str = "O(?????)"
                     remaining = None
+        else:
+            big_o_str = "O(?????)"
+            remaining = None
 
-                remaining_str = format_interval(remaining)
+        remaining_str = format_interval(remaining)
 
-                if len_iterator is not None:
-                    time_percent = elapsed / (remaining + elapsed) if remaining is not None else 0
-                    count_percent = n / len_iterator
-                    percent = time_percent if percent_is_time else count_percent
-                    percent_b = time_percent if bars_is_time else count_percent
+        if len_iterator is not None:
+            time_percent = elapsed / (remaining + elapsed) if remaining is not None else 0
+            count_percent = n / len_iterator
+            percent = time_percent if percent_is_time else count_percent
+            percent_b = time_percent if bars_is_time else count_percent
 
-                    bar_length, frac_bar_length = divmod(
-                        int(percent_b * n_bars * n_syms), n_syms
-                    )
-                    bar_str = UTF[-1] * bar_length
-                    if bar_length < n_bars:  # whitespace padding
-                        bar_str = (
-                            bar_str
-                            + UTF[frac_bar_length]
-                            + UTF[0] * (n_bars - bar_length - 1)
-                        )
+            bar_length, frac_bar_length = divmod(
+                int(percent_b * n_bars * n_syms), n_syms
+            )
+            bar_str = UTF[-1] * bar_length
+            if bar_length < n_bars:  # whitespace padding
+                bar_str = (
+                    bar_str
+                    + UTF[frac_bar_length]
+                    + UTF[0] * (n_bars - bar_length - 1)
+                )
 
-                    s = f"{big_o_str} {percent * 100:3.0f}%|{bar_str}| {n}/{len_iterator} [{elapsed_str}/{remaining_str}, {rate:5.2f}{unit}]"
+            s = f"{big_o_str} {percent * 100:3.0f}%|{bar_str}| {n}/{len_iterator} [{elapsed_str}/{remaining_str}, {rate:5.2f}{unit}]"
 
-                else:
-                    s = f"{big_o_str} ??%|??????????| {n}/{'?'*len(str(n))} [{elapsed_str}/{remaining_str}, {rate:5.2f}{unit}]"
+        else:
+            s = f"{big_o_str} ??%|??????????| {n}/{'?'*len(str(n))} [{elapsed_str}/{remaining_str}, {rate:5.2f}{unit}]"
 
-                len_s = len(s)
-                print('\r' + s + (' ' * max(last_len - len_s, 0)), end="")
-                last_len = len_s
+        len_s = len(s)
+        print('\r' + s + (' ' * max(last_len - len_s, 0)), end="")
+        last_len = len_s
 
-                last_print_n = n
-                last_print_t = now
+        last_print_n = n
+        last_print_t = now
     print()
 
 
