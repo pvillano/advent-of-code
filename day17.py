eeaa37f007e2997517941c8bb1f0d9667e948859
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-17 02:19:53 -0500

    day 17 part 2 raw

--- day17.py
+++ day17.py
@@ -10,6 +10,7 @@ from itertools import (
     permutations,
     combinations,
     pairwise,
+    islice
 )
 from math import sqrt, floor, ceil, gcd, sin, cos, atan2
 
@@ -135,48 +136,63 @@ def part1():
     return total_height
 
 
-def hash_state(rock_idx, moves_idx, stack, view=100):
-    return f"{rock_idx},{moves_idx},{''.join(stack[:view])}"
+def hash_state(rock_idx, moves_idx, stack, view=300):
+    return f"{rock_idx},{moves_idx},{''.join(islice(stack, view))}"
 
 
 def part2():
+    alot = 1000000000000
     rocks = parse_rocks()
 
     lr_to_i = {'<': -1, '>': 1}
 
-    moves = cycle(raw)
-    cave: deque[str] = deque(maxlen=100)
+    move_idx = -1
+    cave: deque[str] = deque(maxlen=1000)
     cave.appendleft("#######")
     total_height = 0
-    for idx, rock in zip(range(2022), cycle(rocks)):
+    history = dict()
+    for num_fallen, rock_idx in zip(range(alot), cycle(range(len(rocks)))):
+
+        state_hash = hash_state(rock_idx, move_idx, cave)
+        if state_hash in history:
+            old_num_fallen, old_total_height = history[state_hash]
+            d_fallen = num_fallen - old_num_fallen
+            d_height = total_height - old_total_height
+            remaining_fallen = alot - num_fallen
+            if remaining_fallen % d_fallen == 0:
+                cycles = remaining_fallen//d_fallen
+                return total_height + cycles * d_height
+        elif num_fallen > 1000:
+            history[state_hash] = num_fallen, total_height
         # debug_print("The", idx+1 ,"rock begins falling:")
         # bottom left
-        if idx == 4:
-            pass
+        rock = rocks[rock_idx]
         x, y = 2, 4
         while True:
             # try moving right/left
-            next_move = next(moves)
+            move_idx = (move_idx + 1) % len(raw)
+            next_move = raw[move_idx]
             new_x, new_y = x + lr_to_i[next_move], y
             overlap = overlaps(new_x, new_y, rock, cave)
             if overlap == "safe":
-                debug_print("Jet of gas pushes rock", next_move)
+                # debug_print("Jet of gas pushes rock", next_move)
                 x, y = new_x, new_y
             else:
                 pass
-                debug_print("Jet of gas pushes rock", next_move, ", but nothing happens")
+                # debug_print("Jet of gas pushes rock", next_move, ", but nothing happens")
 
             new_x, new_y = x, y - 1
             overlap = overlaps(new_x, new_y, rock, cave)
 
             if overlap == "bottom":
-                debug_print("Rock falls 1 unit, causing it to come to rest")
+                # debug_print("Rock falls 1 unit, causing it to come to rest")
                 total_height += add_rock(x, y, rock, cave)
-                debug_print_grid(cave)
+                # debug_print_grid(cave)
                 break
             else:
-                debug_print("Rock falls 1 unit")
+                # debug_print("Rock falls 1 unit")
                 x, y = new_x, new_y
+
     return total_height
 
 if __name__ == "__main__":
