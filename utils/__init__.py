f7ce0ef97d71224ec74001da81197d71cefb8113
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-17 21:17:45 -0500

    start to make utils a proper module

---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+from .std import *
+
+__all__ = [
+    "benchmark",
+    "DEBUG",
+    "debug_print",
+    "debug_print_grid",
+    "debug_print_sparse_grid",
+    "flatten",
+    "get_day",
+    "pipe",
+    "submit"
+]
