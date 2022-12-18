9625e170c0a88904277da882dc580ce0b40157e9
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-18 02:32:14 -0500

    moved template creation into template.py

--- template.py
+++ template.py
@@ -19,8 +19,8 @@ from utils import benchmark, debug_print, debug_print_grid, debug_print_sparse_g
 
 test = """"""
 
-raw = get_day(DAYNUMBER, test)
-lines = raw.split("\n")
+# raw = get_day(DAYNUMBER, test)
+# lines = raw.split("\n")
 
 
 def part1():
@@ -32,5 +32,28 @@ def part2():
 
 
 if __name__ == "__main__":
-    benchmark(part1)
-    benchmark(part2)
+    # benchmark(part1)
+    # benchmark(part2)
+
+####
+    import os
+    import datetime
+    with open("template.py") as template_file:
+        eric_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-5), "EST"))
+
+        # only generate the next day's template if it is close to midnight
+        if eric_time.month == 12:
+            if eric_time.hour < 23:
+                up_to_day = eric_time.day
+            else:
+                up_to_day = eric_time.day + 1
+        else:
+            up_to_day = 25
+        template_string = template_file.read().split("####")[0].replace("# ", "")
+        # inclusive range
+        for i in range(1, up_to_day + 1):
+            p = f"day{i:02}.py"
+            if not os.path.exists(p):
+                file_contents = template_string.replace("DAYNUMBER", str(i))
+                with open(p, "x") as out_file:
+                    out_file.write(file_contents)
--- utils/std.py
+++ utils/std.py
@@ -87,30 +87,3 @@ def submit(answer, day, level, year):
         raise ...
     else:
         raise NotImplementedError()
-
-
-def main():
-    with open("template.py") as template_file:
-        eric_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-5), "EST"))
-
-        # only generate the next day's template if it is close to midnight
-        if eric_time.month == 12:
-            if eric_time.hour < 23:
-                up_to_day = eric_time.day
-            else:
-                up_to_day = eric_time.day + 1
-        else:
-            up_to_day = 25
-
-        template_string = template_file.read()
-        # inclusive range
-        for i in range(1, up_to_day + 1):
-            p = f"day{i:02}.py"
-            if not os.path.exists(p):
-                file_contents = template_string.replace("DAYNUMBER", str(i))
-                with open(p, "x") as out_file:
-                    out_file.write(file_contents)
-
-
-if __name__ == "__main__":
-    main()
