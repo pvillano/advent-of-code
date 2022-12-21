b6933f0c5e070b21e1a6bd4dfe6b18edf6967ae1
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-20 21:02:20 -0500

    back-counting strategy: 22 seconds

--- day19.py
+++ day19.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+import multiprocessing
 import operator
 from collections import defaultdict, deque, Counter
 from copy import copy, deepcopy
@@ -12,6 +14,9 @@ from itertools import (
     pairwise,
 )
 from math import sqrt, floor, ceil, gcd, sin, cos, atan2
+from multiprocessing import Pool
+
+import numpy as np
 
 from otqdm import otqdm
 
@@ -34,66 +39,70 @@ fake_test = """Blueprint 1:
   Each obsidian robot costs 1 ore and 1 clay.
   Each geode robot costs 1 ore and 1 obsidian.""".replace("\n  ", " ")
 
-raw = get_day(19, fake_test)
-lines = raw.split("\n")
+raw = get_day(19, test)
+lines = test.split("\n")
 
 
-def can_build(resources, cost, ):
-    ret = []
-    for r, c in zip(resources, cost):
-        if c > r:
-            return False
-        else:
-            ret.append(r - c)
+def build_within_budget(bluepint, build, budget):
+    return not np.any(bluepint.dot(build) > budget)
+
+
+def one_hot(k):
+    ret = np.zeros(4, int)
+    ret[k] = 1
     return ret
 
 
-def plus(a, b):
-    return tuple(map(sum, zip(a, b)))
+def build_list_iterator(blueprint: np.ndarray, resources: np.ndarray):
+    build_list = np.zeros(4, int)
+    defer = []
+    for big_digit in reversed(range(4)):
+        while build_within_budget(blueprint, np.add(one_hot(big_digit), build_list), resources):
+            build_list[big_digit] += 1
+    yield build_list
+    # count down
+    while any(build_list != np.zeros(4, int)):
+        for borrow_digit in range(4):
+            if build_list[borrow_digit] > 0:
+                build_list[borrow_digit] -= 1
+                break
+        regrew = False
+        for j in reversed(range(borrow_digit)):
+            while build_within_budget(blueprint, one_hot(j) + build_list, resources):
+                build_list[j] += 1
+                regrew = True
+        # if not regrew:
+        #     defer.append(tuple(build_list))
+        # else:
+        yield tuple(build_list)
+    # yield from defer
 
 
 def benchmark_blueprint(blueprint):
     # how many geodes can we extract with these resources?
-    glob_best = (0, 0, 0, 0)
+    glob_best = [0]
 
     @cache
-    def dp(robots=(1, 0, 0, 0), resources=(0, 0, 0, 0), remaining_time=26):
+    def dp(robots=(1, 0, 0, 0), resources=np.zeros(4, int), remaining_time=24):
         # debug_print_recursive(f"{robots=} {resources=} {remaining_time=}")
+        if glob_best[0] == 8:
+            raise OverflowError()
         if remaining_time == 0:
-            return resources[-1]
-        iter_resources = resources
+            tmp = resources[-1]
+            if tmp > glob_best[0]:
+                glob_best[0] = tmp
+                debug_print(f"{resources=} {robots=} {remaining_time=}")
+            return tmp
+
         best = 0
-        for gebot in count():
-            if iter_resources:
-                iter_resources1 = copy(iter_resources)
-                for obbot in count():
-                    if iter_resources1:
-                        iter_resources2 = copy(iter_resources1)
-                        for clbot in count():
-                            if iter_resources2:
-                                iter_resources3 = copy(iter_resources2)
-                                for orbot in count():
-                                    if iter_resources3:
-                                        if obbot > 0:
-                                            pass
-                                        tmp = dp(plus(robots, (orbot, clbot, obbot, gebot)),
-                                                 plus(iter_resources3, robots),
-                                                 remaining_time - 1)
-                                        if tmp > best:
-                                            best = tmp
-                                            # debug_print_recursive(f"{robots=} {resources=} {remaining_time=} {best=}")
-                                    else:
-                                        break
-                                    iter_resources3 = can_build(iter_resources3, blueprint[0])
-                            else:
-                                break
-                            iter_resources2 = can_build(iter_resources2, blueprint[1])
-                    else:
-                        break
-                    iter_resources1 = can_build(iter_resources1, blueprint[2])
-            else:
-                break
-            iter_resources = can_build(iter_resources, blueprint[3])
+        for build in build_list_iterator(blueprint, resources):
+            remaining_resources = resources - blueprint.dot(build)
+            assert not np.any(remaining_resources < 0)
+            new_bots = np.add(robots, build)
+            new_resources = np.add(remaining_resources, robots)
+            candidate = dp(tuple(new_bots), tuple(new_resources), remaining_time - 1)
+            if candidate > best:
+                best = candidate
         return best
 
     return dp()
@@ -105,22 +114,24 @@ def parse():
         int_list = extract_ints(line)
         bp_number, ore_ore, clay_ore, obsidian_ore, obsidian_clay, geode_ore, geode_obsidian = map(int, int_list)
         # ore, clay, obs
-        blueprints[bp_number] = [
+        blueprints[bp_number] = np.array([
             [ore_ore, 0, 0, 0],
             [clay_ore, 0, 0, 0],
             [obsidian_ore, obsidian_clay, 0, 0],
-            [geode_ore, 0, geode_obsidian, 0]]
+            [geode_ore, 0, geode_obsidian, 0]]).T  # whoops
     return blueprints
+
+
+def test():
+    assert len(list(build_list_iterator(np.identity(4, int), np.array([5, 5, 5, 5])))) == 6 ** 4
 
 
 def part1():
     blueprints = parse()
-    best = 0
-    for bp_number, blueprint in blueprints.items():
-        tmp = benchmark_blueprint(blueprint)
-        if tmp > best:
-            best = tmp
-    return best
+    try:
+        return max([bp_number * benchmark_blueprint(blueprint) for bp_number, blueprint in blueprints.items()])
+    except OverflowError:
+        pass
 
 
 def part2():
@@ -128,5 +139,6 @@ def part2():
 
 
 if __name__ == "__main__":
+    test()
     benchmark(part1)
-    benchmark(part2)
+    # benchmark(part2)
