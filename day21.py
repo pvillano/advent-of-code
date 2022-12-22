608d715c83d5e4b6e63b6fdd4e098b0e2ef1b6a3
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-12-21 23:50:47 -0500

    day 21 solve

--- day21.py
+++ day21.py
@@ -1,21 +1,6 @@
 import operator
-from collections import defaultdict, deque, Counter
-from copy import copy, deepcopy
-from functools import cache, lru_cache, partial, reduce
-from itertools import (
-    accumulate,
-    count,
-    cycle,
-    product,
-    permutations,
-    combinations,
-    pairwise,
-)
-from math import sqrt, floor, ceil, gcd, sin, cos, atan2
 
-from otqdm import otqdm
-
-from utils import benchmark, debug_print, debug_print_grid, debug_print_sparse_grid, get_day, pipe, flatten
+from utils import benchmark, get_day, flatten
 
 test = """root: pppw + sjmn
 dbpl: 5
@@ -153,5 +138,5 @@ def part2():
 
 
 if __name__ == "__main__":
-    # benchmark(part1)
+    benchmark(part1)
     benchmark(part2)
