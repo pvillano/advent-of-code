5ab5a70eaa01d9bc40a2bbd7694bc901fc15e952
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-12-03 00:20:01 -0500

    day 3 cleanup

--- day03.py
+++ day03.py
@@ -1,35 +1,23 @@
 import re
+from operator import mul
 
 from utils import benchmark, get_day, test
 
 
-def parse(raw: str):
-    ret = []
-    for line in raw.splitlines():
-        ret.append(line)
-    return ret
-
-
 def part1(raw: str):
-    s = 0
-    for match in re.finditer("mul\\((-?[0-9]+),(-?[0-9]+)\\)",raw):
-        x,y = match.groups()
-        x,y = map(int, [x, y])
-        s += x * y
-    return s
+    return sum(mul(*map(int, match.groups())) for match in re.finditer("mul\\((-?[0-9]+),(-?[0-9]+)\\)", raw))
 
 
 def part2(raw: str):
     s = 0
     doing = True
-    # for match in re.finditer("mul\\((-?[0-9]+),(-?[0-9]+)\\)",raw):
-    for match in re.finditer("(do\\(\\)|don't\\(\\))|(mul\\((-?[0-9]+),(-?[0-9]+)\\))",raw):
-        dodont, mul, x, y = match.groups()
-        if dodont is not None:
-            doing = dodont == 'do()'
+    for match in re.finditer("(do\\(\\)|don't\\(\\))|(mul\\((-?[0-9]+),(-?[0-9]+)\\))", raw):
+        do_or_dont, _, x, y = match.groups()
+        if do_or_dont is not None:
+            doing = do_or_dont == 'do()'
         else:
             if doing:
-                x,y = map(int, [x, y])
+                x, y = map(int, [x, y])
                 s += x * y
 
     return s
@@ -39,7 +27,7 @@ test1 = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5
 
 expected1 = 161
 
-test2 = test1
+test2 = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
 expected2 = 48
 
 
