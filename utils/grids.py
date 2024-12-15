98b854c164780e42921a1d5b07a6eb812ee384de
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-12-15 01:38:45 -0500

    make transpose return a list of lists instead of a list of tuples

--- utils/grids.py
+++ utils/grids.py
@@ -17,7 +17,7 @@ NESW_XY = ((0, 1), (1, 0), (0, -1), (-1, 0))
 def transpose(iterable):
     iterable = list(iterable)
     assert len(set(map(len, iterable))) == 1, "Matrix must not be jagged"
-    return list(zip(*iterable))
+    return list(map(list,zip(*iterable)))
 
 
 def rotate_clockwise(grid):
