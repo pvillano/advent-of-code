846b771989e967ccc846ca492ae07b327054c445
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-12-18 18:57:19 -0500

    17 done

--- day17.py
+++ day17.py
@@ -1,13 +1,7 @@
-import sys
 from dataclasses import dataclass
-from itertools import count
-
-from tqdm import tqdm
 
 from utils import benchmark, get_day, test
-from utils.parallel import starmap16
 from utils.parsing import extract_ints
-from utils.printing import debug_print
 
 
 def parse(raw: str):
@@ -43,7 +37,7 @@ def part1(raw: str):
             case 4:
                 registers['B'] ^= registers['C']
             case 5:
-                out.append(combo%8)
+                out.append(combo % 8)
             case 6:
                 num = registers['A']
                 denom = 1 << combo
@@ -55,45 +49,43 @@ def part1(raw: str):
         ip += 2
     return ','.join(map(str, out))
 
-def work(registers, program, a_iter):
-    old_registers = registers
-    for a in a_iter:
-        registers = dict(old_registers)
 
-        ip = 0
-        out = []
-        while ip in range(len(program)):
-            op = program[ip]
-            literal = program[ip + 1]
-            combo = [0, 1, 2, 3, registers['A'], registers['B'], registers['C']][literal]
-            match op:
-                case 0:
-                    num = registers['A']
-                    denom = 1 << combo
-                    registers['A'] = num // denom
-                case 1:
-                    registers['B'] ^= literal
-                case 2:
-                    registers['B'] = combo % 8
-                case 3:
-                    if registers['A'] != 0:
-                        ip = literal
-                        continue
-                case 4:
-                    registers['B'] ^= registers['C']
-                case 5:
-                    out.append(combo%8)
-                case 6:
-                    num = registers['A']
-                    denom = 1 << combo
-                    registers['B'] = num // denom
-                case 7:
-                    num = registers['A']
-                    denom = 1 << combo
-                    registers['C'] = num // denom
-            ip += 2
-        if out == program:
-            return a
+def work(registers, program):
+    old_registers = registers
+    registers = dict(old_registers)
+    ip = 0
+    out = []
+    while ip in range(len(program)):
+        op = program[ip]
+        literal = program[ip + 1]
+        combo = [0, 1, 2, 3, registers['A'], registers['B'], registers['C']][literal]
+        match op:
+            case 0:
+                num = registers['A']
+                denom = 1 << combo
+                registers['A'] = num // denom
+            case 1:
+                registers['B'] ^= literal
+            case 2:
+                registers['B'] = combo % 8
+            case 3:
+                if registers['A'] != 0:
+                    ip = literal
+                    continue
+            case 4:
+                registers['B'] ^= registers['C']
+            case 5:
+                out.append(combo % 8)
+            case 6:
+                num = registers['A']
+                denom = 1 << combo
+                registers['B'] = num // denom
+            case 7:
+                num = registers['A']
+                denom = 1 << combo
+                registers['C'] = num // denom
+        ip += 2
+    return out
 
 
 # def part2(raw: str):
@@ -104,8 +96,10 @@ def work(registers, program, a_iter):
 class Shifted:
     value: str
     offset: int
+
     def __str__(self):
-        return f"{self.value}[{self.offset}:]"
+        return f"({self.value} >> {self.offset})"
+
 
 def part22(raw: str):
     old_registers, program = parse(raw)
@@ -122,29 +116,31 @@ def part22(raw: str):
             case 0:
                 assert combo == 3
                 if isinstance(registers['A'], Shifted):
-                    registers['A'] = Shifted(registers['A'].value, registers['A'].offset + 1)
+                    registers['A'] = Shifted(registers['A'].value, registers['A'].offset + 3)
                 else:
-                    registers['A'] = f"({registers['A']} << {combo})"
+                    registers['A'] = f"({registers['A']} >> {combo})"
             case 1:
                 registers['B'] = f"({registers['B']} ^ {literal})"
             case 2:
                 if isinstance(combo, Shifted):
-                    registers['B'] = f"{combo.value}[{combo.offset}]"
+                    registers['B'] = f"(({combo.value} >> {combo.offset}) % 8)"
                 else:
-                    registers['B'] = f"{combo}[0]"
+                    registers['B'] = f"{combo} % 8"
             case 3:
                 if len(out_conditions) < len(program):
                     jump_conditions.append(f"{registers['A']} != 0")
-                ip = literal
-                continue
+                    ip = literal
+                    continue
+                else:
+                    jump_conditions.append(f"{registers['A']} == 0")
+                    break
             case 4:
                 registers['B'] = f"({registers['B']} ^ {registers['C']})"
             case 5:
-                out_conditions.append(f"{combo}[0] == {program[len(out_conditions) - 1]}")
+                out_conditions.append(f"{combo} % 8 == {program[len(out_conditions) - 1]}")
                 assert literal in range(4, 8)
                 registers['0123ABC'[literal]] = str(program[len(out_conditions) - 1])
-                if len(out_conditions) == len(program):
-                    break
+                assert len(out_conditions) <= len(program)
             case 6:
                 registers['B'] = f"({registers['A']} >> {combo})"
             case 7:
@@ -155,24 +151,32 @@ def part22(raw: str):
     for i in reversed(out_conditions):
         print(i)
 
+
 def part2(raw):
     part22(raw)
     registers, program = parse(raw)
-    candidates = list(range(2**9))
-    for i in reversed(range(16)):
+    candidates = [0]
+    for i in reversed(range(len(program))):
+        offset = i * 3
         new_candidates = []
         for a0 in candidates:
             for da in range(8):
-                a = a0 + da
-                if (((a % 8) ^ 2 ^ (a >> (a % 8))) ^ 3) % 8 == program[i]:
-                    new_candidates.append(a * 8)
+                a = a0 + (da << offset)
+                if (((((a >> offset) % 8) ^ 2) ^ ((a >> offset) >> (((a >> offset) % 8) ^ 2))) ^ 3) % 8 == program[i]:
+                    new_candidates.append(a)
+        a = min(new_candidates)
+        print(i, work({'A': a, 'B': 0, 'C': 0}, program), a)
         if i == 1:
             pass
         candidates = new_candidates
     a = min(candidates)
+    # assert (((((a >> 45) % 8) ^ 2) ^ ((a >> 45) >> (((a >> 45) % 8) ^ 2))) ^ 3) % 8 == 3
+    assert a >> 48 == 0
     assert a < 4865100593967448
     assert a < 432075869381536
+    assert a < 297770093506464
     return a
+
 
 test1 = """Register A: 729
 Register B: 0
@@ -199,7 +203,6 @@ def main():
     #     1,7,2,1,4,1,5,4,0
     # test(part2, test2, expected2)
     benchmark(part2, raw)
-
 
 
 if __name__ == "__main__":
