ccdb82dd6bdb5d86ba64ab10b27acd7c45da89a8
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-12-18 21:11:58 -0500

    z3

--- day17.py
+++ day17.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+import z3
 
 from utils import benchmark, get_day, test
 from utils.parsing import extract_ints
@@ -14,12 +14,6 @@ def parse(raw: str):
 
 def part1(raw: str):
     registers, program = parse(raw)
-    return ','.join(map(str, work(registers, program)))
-
-
-def work(registers, program):
-    old_registers = registers
-    registers = dict(old_registers)
     ip = 0
     out = []
     while ip in range(len(program)):
@@ -28,9 +22,7 @@ def work(registers, program):
         combo = [0, 1, 2, 3, registers['A'], registers['B'], registers['C']][literal]
         match op:
             case 0:
-                num = registers['A']
-                denom = 1 << combo
-                registers['A'] = num // denom
+                registers['A'] = registers['A'] >> combo
             case 1:
                 registers['B'] ^= literal
             case 2:
@@ -44,102 +36,53 @@ def work(registers, program):
             case 5:
                 out.append(combo % 8)
             case 6:
-                num = registers['A']
-                denom = 1 << combo
-                registers['B'] = num // denom
+                registers['B'] = registers['A'] >> combo
             case 7:
-                num = registers['A']
-                denom = 1 << combo
-                registers['C'] = num // denom
+                registers['C'] = registers['A'] >> combo
         ip += 2
-    return out
+    return ','.join(map(str, out))
 
 
-# def part2(raw: str):
-#     registers, program = parse(raw)
-#     return starmap16(work, [[registers, program, count(offset, 16)] for offset in range(16)])
-
-@dataclass
-class Shifted:
-    value: str
-    offset: int
-
-    def __str__(self):
-        return f"({self.value} >> {self.offset})"
-
-
-def part22(raw: str):
-    old_registers, program = parse(raw)
-    jump_conditions = []
-    out_conditions = []
+def part2(raw):
+    registers, program = parse(raw)
     ip = 0
-    registers = dict(old_registers)
-    registers['A'] = Shifted('a', 0)
-    while ip in range(len(program)):
+    a = z3.BitVec('a', len(program) * 3 + 3)
+    optimizer = z3.Optimize()
+    optimizer.minimize(a)
+    optimizer.add(a > 0)
+    registers['A'] = a
+    times_outputted = 0
+    while True:
         op = program[ip]
         literal = program[ip + 1]
         combo = [0, 1, 2, 3, registers['A'], registers['B'], registers['C']][literal]
         match op:
             case 0:
-                assert combo == 3
-                if isinstance(registers['A'], Shifted):
-                    registers['A'] = Shifted(registers['A'].value, registers['A'].offset + 3)
-                else:
-                    registers['A'] = f"({registers['A']} >> {combo})"
+                registers['A'] = registers['A'] >> combo
             case 1:
-                registers['B'] = f"({registers['B']} ^ {literal})"
+                registers['B'] = registers['B'] ^ literal
             case 2:
-                if isinstance(combo, Shifted):
-                    registers['B'] = f"(({combo.value} >> {combo.offset}) % 8)"
-                else:
-                    registers['B'] = f"{combo} % 8"
+                registers['B'] = combo % 8
             case 3:
-                if len(out_conditions) < len(program):
-                    jump_conditions.append(f"{registers['A']} != 0")
+                if times_outputted < len(program):
+                    optimizer.add(registers['A'] != 0)
                     ip = literal
                     continue
                 else:
-                    jump_conditions.append(f"{registers['A']} == 0")
+                    optimizer.add(registers['A'] == 0)
                     break
             case 4:
-                registers['B'] = f"({registers['B']} ^ {registers['C']})"
+                registers['B'] ^= registers['C']
             case 5:
-                out_conditions.append(f"{combo} % 8 == {program[len(out_conditions) - 1]}")
-                assert literal in range(4, 8)
-                if raw != test2:
-                    registers['0123ABC'[literal]] = str(program[len(out_conditions) - 1])
-                assert len(out_conditions) <= len(program)
+                optimizer.add(combo % 8 == program[times_outputted])
+                times_outputted += 1
             case 6:
-                registers['B'] = f"({registers['A']} >> {combo})"
+                registers['B'] = registers['A'] >> combo
             case 7:
-                registers['C'] = f"({registers['A']} >> {combo})"
+                registers['C'] = registers['A'] >> combo
         ip += 2
-    for i in jump_conditions:
-        print(i)
-    for i in reversed(out_conditions):
-        print(i)
-
-
-def part2(raw):
-    part22(raw)
-    registers, program = parse(raw)
-    candidates = [0]
-    for i in reversed(range(len(program))):
-        offset = i * 3
-        new_candidates = []
-        for a0 in candidates:
-            for da in range(8):
-                a = a0 + (da << offset)
-                if (((((a >> offset) % 8) ^ 2) ^ ((a >> offset) >> (((a >> offset) % 8) ^ 2))) ^ 3) % 8 == program[i]:
-                    new_candidates.append(a)
-        a = min(new_candidates)
-        print(i, work({'A': a, 'B': 0, 'C': 0}, program), a)
-        if i == 1:
-            pass
-        candidates = new_candidates
-    a = min(candidates)
-    assert a >> 48 == 0
-    return a
+    optimizer.check()
+    return optimizer.model().eval(a)
 
 
 test1 = """Register A: 729
@@ -160,11 +103,11 @@ expected2 = 117440
 
 
 def main():
-    test(part1, test1, expected1)
     raw = get_day(17)
+    benchmark(part2, raw)
+    test(part1, test1, expected1)
     benchmark(part1, raw)
-    # test(part2, test2, expected2)
-    part22(test2)
+    test(part2, test2, expected2)
     benchmark(part2, raw)
 
 
