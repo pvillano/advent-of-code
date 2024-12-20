725ee1efc5680e5e9d7db0c94b6d2eae2993b381
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-12-20 01:22:08 -0500

    cleanup

--- day20.py
+++ day20.py
@@ -1,33 +1,21 @@
 from collections import Counter, deque
-from functools import cache
 from itertools import product
 from math import inf
-
-import numpy as np
-import z3
-from tqdm import tqdm
 
 from utils import benchmark, get_day, test
 from utils.grids import grid_index, NEWS_RC, NESW_RC
 from utils.parsing import extract_ints
 from utils.printing import debug_print, debug_print_grid
 
-__ = np, z3, tqdm, extract_ints, cache, Counter, deque
-
 
 def parse(raw: str):
     grid = raw.splitlines()
     start = grid_index(grid, "S")
     end = grid_index(grid, "E")
-    return start, end, grid
-
-
-def part1(raw: str):
-    start, end, grid = parse(raw)
-    height, width = len(grid), len(grid[0])
 
     lch = [[inf] * len(row) for row in grid]
     lch[start[0]][start[1]] = 0
+    height, width = len(grid), len(grid[0])
     q = deque([start])
     while q:
         r, c = q.pop()
@@ -41,6 +29,11 @@ def part1(raw: str):
             if lch[rr][cc] > cost_here + 1:
                 lch[rr][cc] = cost_here + 1
                 q.appendleft((rr, cc))
+    return start, end, grid, lch, width, height
+
+
+def part1(raw: str):
+    start, end, grid, lch, width, height = parse(raw)
     s = Counter()
     for r0, c0 in product(range(height), range(width)):
         if grid[r0][c0] != "#":
@@ -60,29 +53,13 @@ def part1(raw: str):
             if c - start - 2 > 0:
                 s[c - start - 2] += 1
     debug_print(sorted(s.items()))
+    return sum(s.values())
 
 
 def part2(raw: str):
-    start, end, grid = parse(raw)
-    height, width = len(grid), len(grid[0])
-
-    lch = [[inf] * len(row) for row in grid]
-    lch[start[0]][start[1]] = 0
-    q = deque([start])
-    while q:
-        r, c = q.pop()
-        cost_here = lch[r][c]
-        for dr, dc in NESW_RC:
-            rr, cc = r + dr, c + dc
-            if rr not in range(height) or cc not in range(width):
-                continue
-            if grid[rr][cc] == "#":
-                continue
-            if lch[rr][cc] > cost_here + 1:
-                lch[rr][cc] = cost_here + 1
-                q.appendleft((rr, cc))
+    start, end, grid, lch, width, height = parse(raw)
     s = Counter()
-    debug_print_grid(lch)
+    # debug_print_grid(lch)
     for r0, c0 in product(range(1, height - 1), range(1, width - 1)):
         start = lch[r0][c0]
         if start == inf:
@@ -102,9 +79,9 @@ def part2(raw: str):
                 s[c - start - distance] += 1
     debug_print([(k, v) for k, v in sorted(s.items())])
     debug_print([(k, v) for k, v in sorted(s.items()) if k >= 50])
-    ret = sum(c for k, c in s.items() if k >= 100)
-    assert ret != 997284
-    return ret
+    if raw == test1:
+        return sum(c for k, c in s.items() if k >= 50)
+    return sum(c for k, c in s.items() if k >= 100)
 
 
 test1 = """###############
@@ -123,16 +100,53 @@ test1 = """###############
 #...#...#...###
 ###############"""
 
-expected1 = None
+expected1 = sum(
+    extract_ints(
+        """
+    There are 14 cheats that save 2 picoseconds.
+    There are 14 cheats that save 4 picoseconds.
+    There are 2 cheats that save 6 picoseconds.
+    There are 4 cheats that save 8 picoseconds.
+    There are 2 cheats that save 10 picoseconds.
+    There are 3 cheats that save 12 picoseconds.
+    There is one cheat that saves 20 picoseconds.
+    There is one cheat that saves 36 picoseconds.
+    There is one cheat that saves 38 picoseconds.
+    There is one cheat that saves 40 picoseconds.
+    There is one cheat that saves 64 picoseconds.
+""".replace(
+            "one", "1"
+        )
+    )[::2]
+)
 
 test2 = test1
-expected2 = None
+expected2 = sum(
+    extract_ints(
+        """
+    There are 32 cheats that save 50 picoseconds.
+    There are 31 cheats that save 52 picoseconds.
+    There are 29 cheats that save 54 picoseconds.
+    There are 39 cheats that save 56 picoseconds.
+    There are 25 cheats that save 58 picoseconds.
+    There are 23 cheats that save 60 picoseconds.
+    There are 20 cheats that save 62 picoseconds.
+    There are 19 cheats that save 64 picoseconds.
+    There are 12 cheats that save 66 picoseconds.
+    There are 14 cheats that save 68 picoseconds.
+    There are 12 cheats that save 70 picoseconds.
+    There are 22 cheats that save 72 picoseconds.
+    There are 4 cheats that save 74 picoseconds.
+    There are 3 cheats that save 76 picoseconds.
+"""
+    )[::2]
+)
 
 
 def main():
     test(part1, test1, expected1)
     raw = get_day(20)
-    # benchmark(part1, raw)
+    benchmark(part1, raw)
     test(part2, test2, expected2)
     benchmark(part2, raw)
 
