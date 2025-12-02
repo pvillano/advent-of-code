8b535f997693f229f2cd8cdfef07a3acf9187df2
Peter (Rocky) Villano <github@saej.in>
2025-12-01 21:26:31 -0800

    day 2 cleanup

--- 2025/day02.py
+++ 2025/day02.py
@@ -1,6 +1,4 @@
-from itertools import groupby, batched
-
-from tqdm import tqdm
+from itertools import batched
 
 from utils import benchmark, test
 from utils.advent import get_input
@@ -11,32 +9,32 @@ from utils.printing import debug_print
 @degenerate
 def parse(raw: str):
     for line in raw.split(','):
-        a, b = line.split('-')
-        yield int(a), int(b)
+        lower, upper_inclusive = line.split('-')
+        yield int(lower), int(upper_inclusive)
 
 
 def part1(raw: str):
-    sumall = 0
-    for a,b in tqdm(parse(raw)):
-        for i in range(a,b+1):
-            s = str(i)
-            if len(s) % 2 == 0 and s[len(s)//2:] == s[:len(s)//2]:
-                sumall += i
-    return sumall
+    gift_ids_sum = 0
+    for lower, upper_inclusive in parse(raw):
+        for gift_id in range(lower, upper_inclusive + 1):
+            id_str = str(gift_id)
+            if id_str[len(id_str) // 2:] == id_str[:len(id_str) // 2]:
+                gift_ids_sum += gift_id
+    return gift_ids_sum
 
 
 def part2(raw: str):
-    sumall = 0
-    for a,b in (parse(raw)):
-        for i in range(a,b+1):
-            s = str(i)
-            for group_size in range(1, len(s)//2+1):
-                groups = set(batched(s, group_size))
-                if len(groups) == 1:
-                    debug_print(i)
-                    sumall += i
+    gift_ids_sum = 0
+    for lower, upper_inclusive in parse(raw):
+        for gift_id in range(lower, upper_inclusive + 1):
+            id_str = str(gift_id)
+            for chunk_size in range(1, len(id_str) // 2 + 1):
+                unique_chunks = set(batched(id_str, chunk_size))
+                if len(unique_chunks) == 1:
+                    debug_print(gift_id)
+                    gift_ids_sum += gift_id
                     break
-    return sumall
+    return gift_ids_sum
 
 
 test1 = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""
